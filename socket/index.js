8a093ce8130640ae289616288c4706006ac55b44
Mihai Cherechesu <mihai.cherechesu@gmail.com>
2023-01-12 03:26:03 +0200

    Try more and more and more and more stuff
    Signed-off-by: Mihai Cherechesu <mihai.cherechesu@gmail.com>

--- socket/index.js
+++ socket/index.js
@@ -18,25 +18,33 @@ let onlineCouriers = [];
 const addNewUser = (email, role, socketId) => {
   if (role === "courier") {
     !onlineCouriers.some(user => user.email === email) && onlineCouriers.push({email, socketId});
-    process.stdout.write("Online couriers for " + email + ": " + onlineCouriers + "\n");
+    printList("ADDNEWUSER, clients", onlineClients, email);
   } else if (role === "client") {
     !onlineClients.some(user => user.email === email) && onlineClients.push({email, socketId});
-    process.stdout.write("Online clients for " + email + ": " + onlineClients + "\n");
+    printList("ADDNEWUSER, couriers", onlineCouriers, email);
   }
+}
+
+const printList = (event, list, str) => {
+  process.stdout.write("[" + event + "]: for" + str + " [")
+  list.forEach(function(v) {
+    process.stdout.write(v + ", ")
+  })
+  process.stdout.write("]\n")
 }
 
 const removeUser = (socketId) => {
   onlineCouriers = onlineCouriers.filter(user => user.socketId !== socketId);
   onlineClients = onlineClients.filter(user => user.socketId !== socketId);
-  process.stdout.write("[DISCONNECT] Remaining clients for " + socketId + ": " + onlineClients + "\n");
-  process.stdout.write("[DISCONNECT] Remaining couriers for " + socketId + ": " + onlineCouriers + "\n");
+  printList("DISCONNECT, clients", onlineClients, socketId);
+  printList("DISCONNECT, couriers", onlineCouriers, socketId);
 }
 
 const removeUserByEmail = (email) => {
   onlineCouriers = onlineCouriers.filter(user => user.email !== email);
   onlineClients = onlineClients.filter(user => user.email !== email);
-  process.stdout.write("[NEWCONN] Remaining clients for " + email + ": " + onlineClients + "\n");
-  process.stdout.write("[NEWCONN] Remaining couriers for " + email + ": " + onlineCouriers + "\n");
+  printList("NEWCONN, clients", onlineClients, email);
+  printList("NEWCONN, couriers", onlineCouriers, email);
 }
 
 const getUser = (email, role) => {
@@ -49,7 +57,7 @@ nsp.on("connection", (socket) => {
   process.stdout.write("On connection!\n");
 
   socket.on("newUser", ({email, role}) => {
-    process.stdout.write("On newUser: " + email + " " + role + "\n");
+    process.stdout.write("[ONNEWUSER]: " + email + " " + role + "\n");
 
     removeUserByEmail(email);
     addNewUser(email, role, socket.id);
@@ -57,8 +65,8 @@ nsp.on("connection", (socket) => {
 
   socket.on("sendOrder", ({email, orderId}) => {
     process.stdout.write("Received order from " + email + " on order: " + orderId + "\n");
-    process.stdout.write("[RECVORDER] Remaining clients for: " + onlineClients + "\n");
-    process.stdout.write("[RECVORDER] Remaining couriers for: " + onlineCouriers + "\n");
+    printList("RECVORDER, clients", onlineClients, email);
+    printList("RECVORDER, couriers", onlineCouriers, email);
 
     onlineClients.forEach((c) => {
         process.stdout.write("Send order notification to socket: ", c.socketId + " " + c.email + "\n");
